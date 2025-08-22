1741926832ed133948eca585f09268a7389bc201
minseok5408 <minseok5408@gmail.com>
2025-08-23 03:24:37 +0900

    Create jekyll

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755886628',resources: [
+    cacheName: 'chirpy-1755886989',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
