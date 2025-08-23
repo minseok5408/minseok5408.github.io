45cbad7e6f5733bd26e826e7a306a9ced33e1bd9
minseok5408 <minseok5408@gmail.com>
2025-08-23 16:32:45 +0900

    update: 프로필 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1755888146',resources: [
+    cacheName: 'chirpy-1755934347',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
