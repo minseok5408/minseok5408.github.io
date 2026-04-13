6231a750106eca60dd3b71c9819f3e048212575e
Minseok Kim <minseok5408@gmail.com>
2026-04-13 11:13:18 +0900

    댓글기능(utterances) 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1775108109',resources: [
+    cacheName: 'chirpy-1776046276',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
