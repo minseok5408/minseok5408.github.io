03b9c63a61739cffc122aa1d555d6f4dd229be31
Minseok Kim <minseok5408@gmail.com>
2026-04-16 10:14:59 +0900

    마크다운 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1776064408',resources: [
+    cacheName: 'chirpy-1776301938',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
